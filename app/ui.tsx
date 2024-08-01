c8962cc91d8497cff67d13e2f8cfd782bd16adbc
Daco2020 <dacokim32@gmail.com>
2024-08-01 21:35:22 +0900

    NextJS 기본 설정

---
+++ app/ui.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function UI() {
+  return (
+    <div className="mx-auto flex flex-col items-center justify-center h-screen">
+      <p className="text-5xl">너만의 멋진 프로젝트를 만들어봐! 🚀</p>
+    </div>
+  );
+}
