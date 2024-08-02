4bfbfe088a3ce394309767ecffd1e45c51896b02
Daco2020 <dacokim32@gmail.com>
2024-08-02 23:17:11 +0900

    feat: Seo 최적화

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,6 +1,19 @@
 import { getMovieById } from "actions/movieActions";
 import UI from "./ui";
 
+// 개별 페이지에서 metadata 정보를 동적으로 할당
+export async function generateMetadata({ params, searchParams }) {
+  const movie = await getMovieById(params.id);
+
+  return {
+    title: movie.title,
+    description: movie.overview,
+    openGraph: {
+      images: [movie.image_url],
+    },
+  };
+}
+
 export default async function MovieDetail({ params }) {
   const movie = await getMovieById(params.id);
 
