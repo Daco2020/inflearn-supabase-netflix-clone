3799df04c7301aaccac58c9fa84d3f85de9b898c
Daco2020 <dacokim32@gmail.com>
2024-08-02 16:56:17 +0900

    chore: supabase 스토리지 보일러플레이트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
 import UI from "./ui";
 
+// metadata 정보를 추가하려면 UI 컴포넌트를 별도로 분리하는 것이 좋다.
+// use client 는 metadata 를 사용하지 못하기 때문이다.
+export const metadata = {
+  title: "Create Next App",
+  description: "Create Next App",
+};
+
 export default function Home() {
-  return (
-    <main>
-      <UI></UI>
-    </main>
-  );
+  return <UI />;
 }
--- app/ui.tsx
+++ app/ui.tsx
@@ -1,9 +1,5 @@
 "use client";
 
 export default function UI() {
-  return (
-    <div className="mx-auto flex flex-col items-center justify-center h-screen">
-      <p className="text-5xl">너만의 멋진 프로젝트를 만들어봐! 🚀</p>
-    </div>
-  );
+  return <main className="w-full p-4">프로젝트 시작</main>;
 }
