b6adb071634b9d4b4dd4844e832f4cba8b92cd2c
Daco2020 <dacokim32@gmail.com>
2024-08-02 16:57:11 +0900

    chore: netflix 프로젝트 설정

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import UI from "./ui";
 // metadata 정보를 추가하려면 UI 컴포넌트를 별도로 분리하는 것이 좋다.
 // use client 는 metadata 를 사용하지 못하기 때문이다.
 export const metadata = {
-  title: "Create Next App",
-  description: "Create Next App",
+  title: "TMDBFLIX",
+  description: "TMDBFLIX",
 };
 
 export default function Home() {
