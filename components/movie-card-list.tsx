818abdf3e04682b03c81aaa2e16eae9c0a1b3f08
Daco2020 <dacokim32@gmail.com>
2024-08-02 23:13:29 +0900

    feat: 무한스크롤 구현

--- components/movie-card-list.tsx
+++ components/movie-card-list.tsx
@@ -1,26 +1,54 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import MovieCard from "./movie-card";
 import { searchMovies } from "actions/movieActions";
 import { Spinner } from "@material-tailwind/react";
 import { useRecoilValue } from "recoil";
 import { searchState } from "utils/recoil/atoms";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 export default function MovieCardList() {
   const search = useRecoilValue(searchState);
-  const searchMoviesQuery = useQuery({
-    queryKey: ["movie", search],
-    queryFn: () => searchMovies(search),
+  const { data, isFetching, isFetchingNextPage, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      initialPageParam: 1,
+      queryKey: ["movie", search],
+      queryFn: ({ pageParam }) =>
+        searchMovies({ search, page: pageParam, pageSize: 12 }),
+      getNextPageParam: (lastPage) =>
+        lastPage.page ? lastPage.page + 1 : null,
+    });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching && !isFetchingNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage]);
+
+  useEffect(() => {
+    console.log("inView", inView);
+  }, [inView]);
 
   return (
     <div className="grid gap-2 md:grid-cols-4 grid-cols-3 w-full h-full">
-      {searchMoviesQuery.isPending && <Spinner />}
-      {searchMoviesQuery.data &&
-        searchMoviesQuery.data.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+      {(isFetching || isFetchingNextPage) && <Spinner />}
+      {
+        <>
+          {data?.pages
+            ?.map((page) => page.data)
+            ?.flat()
+            ?.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          <div ref={ref}></div> {/* 끝까지 내리면 inView 가 true로 바뀐다. */}
+        </>
+      }
     </div>
   );
 }
